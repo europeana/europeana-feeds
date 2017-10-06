6317db3688a63d186708a3bae6671bcaf66f866e
Lutz Biedinger <lutz.biedinger@gmail.com>
2017-10-06 10:22:59 +0200

    initial commit

---
+++ lib/europeana/feed/jobs.rb
@@ -0,0 +1,9 @@
+require "europeana/feed/jobs/version"
+
+module Europeana
+  module Feed
+    module Jobs
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/europeana/feed/jobs/version.rb
@@ -0,0 +1,7 @@
+module Europeana
+  module Feed
+    module Jobs
+      VERSION = "0.1.0"
+    end
+  end
+end
