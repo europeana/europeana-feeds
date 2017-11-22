4216633716da140e0388984fd0eca05e4a6d5568
Lutz Biedinger <lutz.biedinger@gmail.com>
2017-11-22 12:56:11 +0100

    prefer last_built over last_modified

--- app/jobs/europeana/feeds/fetch_job.rb
+++ app/jobs/europeana/feeds/fetch_job.rb
@@ -13,10 +13,23 @@ module Europeana
         feed_cache_key = "feed/#{@url}"
         cached_feed = Rails.cache.fetch(feed_cache_key)
 
-        if cached_feed.blank? || cached_feed.last_modified != @feed.last_modified
+        if modified?(cached_feed)
           Rails.cache.write(feed_cache_key, @feed)
           @updated = true
         end
+      end
+
+      def modified?(cached_feed)
+        return true if cached_feed.blank?
+
+        if @feed.last_built
+          return cached_feed.last_built != @feed.last_built
+        elsif @feed.last_modified
+          return cached_feed.last_modified != @feed.last_modified
+        end
+        # When the feed doesn't specify a build date or modified date,
+        # assume it's been modified.
+        true
       end
     end
   end
--- spec/jobs/europeana/feeds/fetch_job_spec.rb
+++ spec/jobs/europeana/feeds/fetch_job_spec.rb
@@ -65,14 +65,59 @@ RSpec.describe Europeana::Feeds::FetchJob do
       end
     end
 
-    context 'when the last_modified date has changed' do
+    context 'when the last_built date has changed' do
       before do
-        Rails.cache.write(cache_key, ::Feedjira::Feed.parse(rss_body.gsub('Mon, 22 May 2017', 'Tue, 23 May 2017')))
+        Rails.cache.write(cache_key, ::Feedjira::Feed.parse(rss_body.gsub('<lastBuildDate>Mon, 22 May 2017', '<lastBuildDate>Tue, 23 May 2017')))
       end
 
       it 'should update the cache and @updated should be true' do
         expect { subject.perform(url) }.to change { Rails.cache.fetch(cache_key) }
         expect(subject.instance_variable_get(:@updated)).to eq(true)
+      end
+    end
+
+    context 'when the feed does NOT have a build date' do
+      let(:rss_body) do
+        <<~FEEDXML
+      <?xml version="1.0"?>
+      <rss version="2.0">
+      <channel>
+        <title>Example Channel</title>
+        <link>http://example.com/</link>
+        <description>My example channel</description>
+        <item>
+           <title>Example item</title>
+           <link>http://example.com/item</link>
+           <description>About the example item...</description>
+           <content:encoded><![CDATA[<img src="http://www.example.com/image.png"/>]]></content:encoded>
+           <pubDate>Mon, 22 May 2017 00:00:00 +0000</pubDate>
+        </item>
+      </channel>
+      </rss>
+        FEEDXML
+      end
+
+      context 'when the last_modified date has NOT changed' do
+        before do
+          Rails.cache.write(cache_key, ::Feedjira::Feed.parse(rss_body))
+        end
+
+        it 'should not update the cache and @updated should be false' do
+          expect(Rails.cache).to_not receive(:write)
+          subject.perform(url)
+          expect(subject.instance_variable_get(:@updated)).to_not eq(true)
+        end
+      end
+
+      context 'when the last_modified date has changed' do
+        before do
+          Rails.cache.write(cache_key, ::Feedjira::Feed.parse(rss_body.gsub('<pubDate>Mon, 22 May 2017', '<pubDate>Tue, 23 May 2017')))
+        end
+
+        it 'should update the cache and @updated should be true' do
+          expect { subject.perform(url) }.to change { Rails.cache.fetch(cache_key) }
+          expect(subject.instance_variable_get(:@updated)).to eq(true)
+        end
       end
     end
   end
